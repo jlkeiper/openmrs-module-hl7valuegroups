4ff55ea6655e1b7984c1c181795234931745d2e8
Jeremy Keiper <jeremy@openmrs.org>
2013-01-23 15:36:40 -0500

    added form for submitting an HL7 by uploading a file

--- omod/src/main/java/org/openmrs/module/hl7valuegroups/web/controller/HL7ValueGroupsManageController.java
+++ omod/src/main/java/org/openmrs/module/hl7valuegroups/web/controller/HL7ValueGroupsManageController.java
@@ -13,24 +13,54 @@
  */
 package org.openmrs.module.hl7valuegroups.web.controller;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.context.Context;
+import org.openmrs.hl7.HL7InQueue;
+import org.openmrs.hl7.HL7Service;
+import org.openmrs.hl7.HL7Source;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 /**
  * The main controller.
  */
 @Controller
+@RequestMapping(value = "/module/hl7valuegroups/manage")
 public class  HL7ValueGroupsManageController {
 	
 	protected final Log log = LogFactory.getLog(getClass());
 	
-	@RequestMapping(value = "/module/hl7valuegroups/manage", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public void manage(ModelMap model) {
-		model.addAttribute("user", Context.getAuthenticatedUser());
+	}
+
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void apply(@RequestParam("hl7") MultipartFile hl7) {
+		if (hl7 == null || hl7.isEmpty())
+			return;
+
+		log.warn("processing hl7 ...");
+		HL7Service hl7Service = Context.getHL7Service();
+
+		HL7InQueue queue = new HL7InQueue();
+		queue.setHL7Source(hl7Service.getHL7Source(1));
+		try {
+			queue.setHL7Data(new String(hl7.getBytes()));
+		} catch (IOException e) {
+			log.warn("could not create hl7 from this data");
+			return;
+		}
+
+		hl7Service.saveHL7InQueue(queue);
+		log.warn("hl7 saved ...");
 	}
 }
--- omod/src/main/webapp/manage.jsp
+++ omod/src/main/webapp/manage.jsp
@@ -3,6 +3,11 @@
 
 <%@ include file="template/localHeader.jsp"%>
 
-<p>Hello ${user.systemId}!</p>
+<p>Use the form below to pick a file to submit to the HL7 queue.</p>
+
+<form method="post" enctype="multipart/form-data">
+    <input type="file" name="hl7" />
+    <input type="submit"/>
+</form>
 
 <%@ include file="/WEB-INF/template/footer.jsp"%>
